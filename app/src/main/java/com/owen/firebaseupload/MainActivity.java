b93a95d9a840455b64c41c820f492035596d4130
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-03-13 16:00:54 -0400

    Update MainActivity.java

--- app/src/main/java/com/owen/firebaseupload/MainActivity.java
+++ app/src/main/java/com/owen/firebaseupload/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     
+    private static final int PICK_IMAGE_REQUEST = 1;
+    
     private Button mButtonUpload, mButtonChoose;
     private TextView mtxtViewShowUpload;
     private EditText metFilename;
@@ -17,5 +19,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+            mButtonChoose = (Button)findViewById(R.id.btn_chooseFile);
+        mButtonUpload = (Button)findViewById(R.id.btn_upload);
+        mtxtViewShowUpload = (TextView)findViewById(R.id.tv_show_uploads);
+        metFilename = (EditText)findViewById(R.id.et_choose_file_name);
+        mImageView = (ImageView)findViewById(R.id.image_view);
+        mProgressBar = (ProgressBar)findViewById(R.id.progress_Bar);
+
+        mButtonChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openImageChooser();
+
+            }
+        });
+
+        mtxtViewShowUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mButtonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void openImageChooser()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+
+            mImageUri = data.getData();
+
+//            Picasso.with(this).load(mImageUri).into(mImageView);
+            mImageView.setImageURI(mImageUri);
+
+        }
+    }
     }
 }
